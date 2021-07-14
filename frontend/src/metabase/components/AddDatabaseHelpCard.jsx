1b9e77f08db2bc0ba7f71522ef97298e45176357
Jeff Evans <jeff.evans@metabase.com>
2021-07-14 17:16:29 -0500

    Add support for driver deprecation
    Frontend changes
    
    Calculate supersededBy and supersedes maps from the "superseded-by" property for each engine
    
    Change the options for the engine field to use a function to dynamically show the legacy driver if allowed by rules
    
    Update help card to show warning dialogs for deprecation

--- frontend/src/metabase/components/AddDatabaseHelpCard.jsx
+++ frontend/src/metabase/components/AddDatabaseHelpCard.jsx
@@ -8,11 +8,15 @@ import MetabaseSettings from "metabase/lib/settings";
 import ExternalLink from "metabase/components/ExternalLink";
 import Icon from "metabase/components/Icon";
 
+// TODO: is this really the best way?
+import { allEngines, engineSupersedesMap } from "../entities/databases/forms";
+
 import {
   CardContent,
   HelpCardContainer,
   IconContainer,
 } from "./AddDatabaseHelpCard.styled";
+import Warnings from "metabase/query_builder/components/Warnings";
 
 export const ENGINE_DOCS = {
   bigquery: MetabaseSettings.docsUrl("administration-guide/databases/bigquery"),
@@ -49,43 +53,108 @@ function AddDatabaseHelpCard({ engine, hasCircle = true, ...props }) {
   const docsLink = ENGINE_DOCS[engine] || GENERAL_DB_DOC;
   const shouldDisplayHelpLink = MetabaseSettings.isHosted();
 
+  const supersededBy = engineSupersedesMap["superseded_by"][engine];
+  const supersedes = engineSupersedesMap["supersedes"][engine];
+
+  console.log(
+    "engine: %s, superseded_by: %s, supersedes: %s",
+    engine,
+    supersededBy,
+    supersedes,
+  );
+
   return (
-    <HelpCardContainer p={2} {...props}>
-      <IconContainer
-        align="center"
-        justify="center"
-        className="flex-no-shrink circular"
-        hasCircle={hasCircle}
-      >
-        <Icon size={20} name="database" className="text-brand" />
-      </IconContainer>
-      <CardContent
-        flexDirection="column"
-        justify="center"
-        className="ml2"
-        shouldDisplayHelpLink={shouldDisplayHelpLink}
-      >
-        <div>
-          <p className="text-medium m0">
-            {t`Need help setting up`} {displayName}?
-          </p>
-          <ExternalLink href={docsLink} className="text-brand text-bold">
-            {t`Our docs can help.`}
-          </ExternalLink>
-        </div>
-        {shouldDisplayHelpLink && (
-          <p className="mt2 text-medium m0">
-            {jt`Docs weren't enough?`}{" "}
-            <ExternalLink
-              href={CLOUD_HELP_URL}
-              className="text-brand text-bold"
-            >
-              {t`Write us.`}
+    <div>
+      <HelpCardContainer p={2} {...props}>
+        <IconContainer
+          align="center"
+          justify="center"
+          className="flex-no-shrink circular"
+          hasCircle={hasCircle}
+          x
+        >
+          <Icon size={20} name="database" className="text-brand" />
+        </IconContainer>
+        <CardContent
+          flexDirection="column"
+          justify="center"
+          className="ml2"
+          shouldDisplayHelpLink={shouldDisplayHelpLink}
+        >
+          <div>
+            <p className="text-medium m0">
+              {t`Need help setting up`} {displayName}?
+            </p>
+            <ExternalLink href={docsLink} className="text-brand text-bold">
+              {t`Our docs can help.`}
             </ExternalLink>
-          </p>
-        )}
-      </CardContent>
-    </HelpCardContainer>
+          </div>
+          {shouldDisplayHelpLink && (
+            <p className="mt2 text-medium m0">
+              {jt`Docs weren't enough?`}{" "}
+              <ExternalLink
+                href={CLOUD_HELP_URL}
+                className="text-brand text-bold"
+              >
+                {t`Write us.`}
+              </ExternalLink>
+            </p>
+          )}
+        </CardContent>
+      </HelpCardContainer>
+      {supersedes && (
+        <HelpCardContainer p={2} {...props}>
+          <Warnings
+            className="mx2 align-self-end text-gold"
+            warnings={[t`New driver`]}
+            size={20}
+          />
+
+          <CardContent
+            flexDirection="column"
+            justify="center"
+            className="ml2"
+            shouldDisplayHelpLink={shouldDisplayHelpLink}
+          >
+            <div>
+              <p className="text-medium m0">
+                {t`This driver replaces the legacy version, which is called ${
+                  allEngines[supersedes]["driver-name"]
+                }.
+                If you need to use the legacy driver, you can select it now.  Please let us know if you have any issues
+                with this new driver.`}
+              </p>
+            </div>
+          </CardContent>
+        </HelpCardContainer>
+      )}
+      {supersededBy && (
+        <HelpCardContainer p={2} {...props}>
+          <Warnings
+            className="mx2 align-self-end text-gold"
+            warnings={[t`Driver deprecated`]}
+            size={20}
+          />
+
+          <CardContent
+            flexDirection="column"
+            justify="center"
+            className="ml2"
+            shouldDisplayHelpLink={shouldDisplayHelpLink}
+          >
+            <div>
+              <p className="text-medium m0">
+                {t`This driver is a legacy driver, and will eventually be removed from Metabase.  Please use the newer
+                version, which is called ${
+                  allEngines[supersededBy]["driver-name"]
+                }. Please let us know if you have any
+                issues with this new driver.`}
+              </p>
+            </div>
+          </CardContent>
+        </HelpCardContainer>
+      )}
+    </div>
   );
 }
 
